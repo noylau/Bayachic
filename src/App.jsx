e0e45311731d293c06699d13adde838b5c71c0af
noylau <hualong49@gmail.com>
2025-04-15 16:00:55 +0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import Products from './pages/Products'
+import CustomServices from './pages/CustomServices'
+import About from './pages/About'
+import Contact from './pages/Contact'
+
+function App() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/custom-services" element={<CustomServices />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
